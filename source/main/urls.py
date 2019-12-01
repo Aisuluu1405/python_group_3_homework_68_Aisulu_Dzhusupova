c87b4eeda8d55e3c9c6110c7117fc720af1a42df
Айсулу <aisuluueco2009@yandex.ru>
2019-12-01 20:07:51 +0600

    Пример работы API без работы с базой.

--- source/main/urls.py
+++ source/main/urls.py
@@ -15,7 +15,10 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path
+from webapp.views import api_example
 
 urlpatterns = [
     path('admin/', admin.site.urls),
+    path('echo/', api_example, name='echo_api'),
+
 ]
